f81f8abdaefea98313f2dd3c9c6cb2d90300a0db
Thomas Meeus <thomas.meeus@kunstmaan.be>
2014-09-10 20:34:33 +0200

    berks vendor is deprecated

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,14 @@ sudo softwareupdate -i -a
 sudo gem install bundler
 sudo mkdir /opt
 sudo chown $USER:staff /opt
-git clone https://github.com/thomasmeeus/workstation.git /opt/workstation
+
+if [ ! -e /opt/workstation ]
+then
+    git clone https://github.com/thomasmeeus/workstation.git /opt/workstation
+fi
+
 cd /opt/workstation
-bundle install --path vendor/bundle
+git pull
+bundle vendor --path vendor/bundle
 bundle exec berks install --path vendor/cookbooks
 bundle exec soloist
