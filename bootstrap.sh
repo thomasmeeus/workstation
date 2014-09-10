385f5a5ecf975c7fca86d9213135e1b91784d675
Thomas Meeus <thomas.meeus@kunstmaan.be>
2014-09-10 19:52:33 +0200

    Autoinstall commandline tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo -v
+
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 sudo softwareupdate -i -a
 sudo gem install bundler
 git clone https://github.com/thomasmeeus/workstation.git /opt
