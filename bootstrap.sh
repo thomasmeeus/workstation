b0e9d3ea3b61c7ee065c6d4535f152d4a5cb0556
Thomas Meeus <thomas.meeus@kunstmaan.be>
2014-09-11 14:45:22 +0200

    Extend sudo timeout

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+function sudo_ping() {
+    if [[ ! -z $SUDO_PID ]]; then
+        if [[ $1 -eq stop ]]; then
+            kill $SUDO_PID
+            return
+        else
+            return
+        fi
+    fi
+
+    sudo -v
+    if [[ $? -eq 1 ]]; then
+        echo "Oops, wrong password."
+        return
+    fi
+
+    while true; do
+        sudo -v
+        sleep 1
+    done &
+    SUDO_PID=$!
+
+    # Make sure we don't orphan our pinger
+    trap "sudo_ping stop" 0
+    trap "exit 2" 1 2 3 15
+}
+
+
 sudo_ping
 sleep 5
 
@@ -18,41 +46,3 @@ bundle install --path vendor/bundle
 rm -rf ~/.workstation/vendor/cookbooks
 bundle exec berks vendor vendor/cookbooks
 bundle exec soloist
-
-
-
-
-
-
-function sudo_ping() {
-    if [[ ! -z $SUDO_PID ]]; then
-        if [[ $1 -eq stop ]]; then
-            echo "Stopping sudo ping in PID = $SUDO_PID"
-            kill $SUDO_PID
-            return
-        else
-            echo "Already sudo pinging in PID = $SUDO_PID"
-            return
-        fi
-    fi
-
-    echo "Starting background sudo ping..."
-    sudo -v
-    if [[ $? -eq 1 ]]; then
-        echo "Oops, wrong password."
-        return
-    fi
-    sudo echo "ok"
-
-    while true; do
-        echo 'Sudo ping!'
-        sudo -v
-        sleep 1
-    done &
-    SUDO_PID=$!
-    sudo echo "Sudo pinging in PID = $SUDO_PID"
-
-    # Make sure we don't orphan our pinger
-    trap "sudo_ping stop" 0
-    trap "exit 2" 1 2 3 15
-}
