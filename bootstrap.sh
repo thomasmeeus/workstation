89b7d6f8fab77ff2b4c823dda52a1b480c0e6c2f
Thomas Meeus <thomas.meeus@kunstmaan.be>
2014-09-16 11:13:27 +0200

    Sudo ping doesn't work reliable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
 
-function sudo_ping() {
-    if [[ ! -z $SUDO_PID ]]; then
-        if [[ $1 -eq stop ]]; then
-            kill $SUDO_PID
-            return
-        else
-            return
-        fi
-    fi
-
-    sudo -v
-    if [[ $? -eq 1 ]]; then
-        echo "Oops, wrong password."
-        return
-    fi
-
-    while true; do
-        sudo -v
-        sleep 1
-    done &
-    SUDO_PID=$!
-
-    # Make sure we don't orphan our pinger
-    trap "sudo_ping stop" 0
-    trap "exit 2" 1 2 3 15
-}
-
-
-sudo_ping
-sleep 5
-
 touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 sudo softwareupdate -i -a
 sudo gem install bundler
